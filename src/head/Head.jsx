15464b7aa4859774af8672dca57b7832fb88c77e
guardinrgb <guardin302@gmail.com>
2025-02-20 23:37:03 +0300

    <title>       Профиль     </title>

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -8,7 +8,7 @@ import messages from './messages';
 const Head = ({ intl }) => (
   <Helmet>
     <title>
-      Profile
+      Профиль
     </title>
     <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
   </Helmet>
